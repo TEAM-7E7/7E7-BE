133a5fb7f8c7b1d07defc9729be8153a8ef3e088
imsunyong <imsunyong@naver.com>
2022-06-27 23:47:49 +0900

    (Fix) 배포자동화 🐛

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,7 +3,7 @@
 REPOSITORY=/home/ubuntu/
 cd $REPOSITORY
 
-APP_NAME=selldio
+APP_NAME=marketclip
 JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep 'SNAPSHOT.jar' | tail -n 1)
 JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
 
