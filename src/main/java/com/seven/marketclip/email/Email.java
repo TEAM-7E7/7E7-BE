6cf5fdb1cd6f29176b48c7a824e62f3869f0cf04
imsunyong <imsunyong@naver.com>
2022-07-01 03:22:54 +0900

    #9 (Feat) Exceptions 생성 및 정리 ✨

--- src/main/java/com/seven/marketclip/email/Email.java
+++ src/main/java/com/seven/marketclip/email/Email.java
@@ -1,16 +1,18 @@
 package com.seven.marketclip.email;
 
 import com.seven.marketclip.Timestamped;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.ColumnDefault;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
 
-@Entity (name = "email_validation")
+@Entity(name = "email_validation")
 @Getter
 @NoArgsConstructor
-public class Email extends Timestamped {
+public class Email {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -19,18 +21,27 @@ public class Email extends Timestamped {
     @Column(unique = true)
     private String userEmail;
 
-    private String token;
+    private String authToken;
 
-    @ColumnDefault("0")
-    private Integer count;
+//    @ColumnDefault("0")
+//    private Integer count;
 
-    public Email(EmailDTO emailDTO){
+    private LocalDateTime expireDate;
+
+    @Builder
+    public Email(String userEmail, String authToken) {
+        this.userEmail = userEmail;
+        this.authToken = authToken;
+        this.expireDate = LocalDateTime.now().plusMinutes(10);
+    }
+
+    public Email(EmailDTO emailDTO) {
         this.userEmail = emailDTO.getEmail();
-        this.token = emailDTO.getToken();
+        this.authToken = emailDTO.getAuthToken();
     }
 
-    public void updateCount(){
-        count += 1;
-    }
+//    public void updateCount(){
+//        count += 1;
+//    }
 
 }
