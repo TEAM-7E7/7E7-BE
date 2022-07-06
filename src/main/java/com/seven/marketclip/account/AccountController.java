f1167d25ecd23e03ad54874de030487d3424c654
imsunyong <imsunyong@naver.com>
2022-07-06 18:25:36 +0900

    #6 (Feat) 게시판 CR 및 병합 완료 🔀

--- src/main/java/com/seven/marketclip/account/AccountController.java
+++ src/main/java/com/seven/marketclip/account/AccountController.java
@@ -9,6 +9,9 @@ import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @Api(tags = "유저 컨트롤러")
