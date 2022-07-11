32c9517168ecd246b45409449a450772ad6b7334
sulbin <tlsdnr1135@naver.com>
2022-07-12 02:36:30 +0900

    (Feat) JWT 예외처리 ✨

--- src/main/java/com/seven/marketclip/security/jwt/HeaderTokenExtractor.java
+++ src/main/java/com/seven/marketclip/security/jwt/HeaderTokenExtractor.java
@@ -3,6 +3,8 @@ package com.seven.marketclip.security.jwt;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.util.NoSuchElementException;
 
 @Component
@@ -12,9 +14,9 @@ public class HeaderTokenExtractor {
      * HEADER_PREFIX
      * header Authorization token 값의 표준이되는 변수
      */
-    public final String HEADER_PREFIX = "Bearer ";
+    public final String HEADER_PREFIX = "BEARER ";
 
-    public String extract(String header, HttpServletRequest request) {
+    public String extract(String header, HttpServletRequest request, HttpServletResponse response) {
         /*
          * - Token 값이 올바르지 않은경우 -
          * header 값이 비어있거나 또는 HEADER_PREFIX 값보다 짧은 경우
@@ -22,6 +24,12 @@ public class HeaderTokenExtractor {
          */
         if (header == null || header.equals("") || header.length() < HEADER_PREFIX.length()) {
             System.out.println("error request : " + request.getRequestURI());
+            try {
+                response.getWriter().println("Token - WrongToken");
+                response.setStatus(400);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
             throw new NoSuchElementException("올바른 JWT 정보가 아닙니다.");
         }
 
