ec7881f593e930a160d119ac29f235ab8edffb98
sulbin <tlsdnr1135@naver.com>
2022-07-15 18:31:08 +0900

    sulbin - commit

--- src/main/java/com/seven/marketclip/account/oauth/OauthHandler.java
+++ src/main/java/com/seven/marketclip/account/oauth/OauthHandler.java
@@ -52,6 +52,7 @@ public class OauthHandler implements AuthenticationSuccessHandler {
         response.addHeader(FormLoginSuccessHandler.REFRESH_HEADER, FormLoginSuccessHandler.TOKEN_TYPE + " " + refresh);
         response.sendRedirect("http://localhost:3000?X-ACCESS-TOKEN="+FormLoginSuccessHandler.TOKEN_TYPE + " " + token+"&"+"X-REFRESH-TOKEN="+FormLoginSuccessHandler.TOKEN_TYPE + " " + refresh);
 //        RedirectAttributes redirectAttributes = new
+//        getR
 
     }
 }
