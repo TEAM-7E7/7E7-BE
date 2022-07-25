ed5f6fa5c43891d235c228d9fd59ba4f485eae2d
sulbin <tlsdnr1135@naver.com>
2022-07-25 21:20:06 +0900

    (Fix) JWT 필터 예외처리 수정 🐛

--- src/main/java/com/seven/marketclip/security/JwtFailFilter.java
+++ src/main/java/com/seven/marketclip/security/JwtFailFilter.java
@@ -15,21 +15,39 @@ import java.io.IOException;
 public class JwtFailFilter implements AuthenticationFailureHandler {
     @Override
     public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
-//        System.out.println("진입 성공");
         response.setCharacterEncoding("utf-8");
         response.setStatus(400);
         response.getWriter().print("JWT 문제 생김");
-//        if(exception.getMessage().equals("RefreshFilter-Refresh-invalid_expired_not_existDB")){
-//            System.out.println("리프레쉬 필터 - 리프레쉬 토큰 - 잘못됨");
-//            response.getWriter().println(exception.getMessage());
-//
-//        }else if(exception.getMessage().equals("AccessToken - No Header")){
-//            System.out.println("리프레쉬 필터 - 리프레쉬 토큰 - 잘못됨");
-//            response.getWriter().println(exception.getMessage());
-//
-//        }else if(){
-//
-//        }
+
+        if(exception.getMessage().equals("AccessToken - No Header")){
+            response.getWriter().print("AccessToken - No Header");
+            response.setStatus(400);
+
+        }else if(exception.getMessage().equals("JwtAuthFilter-Refresh-No_Header")){
+            response.getWriter().print("JwtAuthFilter-Refresh-No_Header");
+            response.setStatus(400);
+
+        }else if(exception.getMessage().equals("JwtAuthFilter-Access-Invalid")) {
+            response.getWriter().print("JwtAuthFilter-Access-Invalid");
+            response.setStatus(400);
+
+        }else if(exception.getMessage().equals("JwtAuthFilter-Access-expired")) {
+            response.getWriter().print("JwtAuthFilter-Access-expired");
+            response.setStatus(400);
+
+        }else if(exception.getMessage().equals("JwtAuthFilter-Refresh-Invalid")){
+            response.getWriter().print("JwtAuthFilter-Refresh-Invalid");
+            response.setStatus(400);
+
+        }else if(exception.getMessage().equals("JwtAuthFilter-Refresh-expired")){
+            response.getWriter().print("JwtAuthFilter-Refresh-expired");
+            response.setStatus(400);
+
+        }else if(exception.getMessage().equals("JwtAuthFilter-Refresh-Not_existDB")){
+            response.getWriter().print("JwtAuthFilter-Refresh-Not_existDB");
+            response.setStatus(400);
+
+        }
 
     }
 }
