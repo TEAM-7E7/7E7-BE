58ce356a66e6327a9c3fa780bc296c82fe7d2cd5
KimjaehoLy <kjaeho8389@naver.com>
2022-07-27 19:13:24 +0900

    (Test) 채팅방 전송 테스트 ✅

--- src/main/java/com/seven/marketclip/chat/controller/ChatMessageController.java
+++ src/main/java/com/seven/marketclip/chat/controller/ChatMessageController.java
@@ -6,6 +6,7 @@ import com.seven.marketclip.chat.service.ChatRoomService;
 import com.seven.marketclip.chat.subpub.RedisPublisher;
 import com.seven.marketclip.security.UserDetailsImpl;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,7 +15,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-
+@Slf4j
 @RequiredArgsConstructor
 @RestController
 public class ChatMessageController {
@@ -35,6 +36,7 @@ public class ChatMessageController {
     // websocket "/pub/chat/message"로 들어오는 메시징을 처리한다.
     @MessageMapping("/chat/message")            //메세지 전송
     public void message(ChatMessageReq message) {
+        log.info("여기역이");
         redisPublisher.publish(chatRoomService.getTopic(message.getChatRoomId()), message);
     }
     @PostMapping("/api/chat-message-list")       //메세지 전체 내역 불러오기 및 읽음 처리
