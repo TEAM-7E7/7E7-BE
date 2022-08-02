9a587dbcd43e80478667a34afeda755fb7de1622
KimjaehoLy <kjaeho8389@naver.com>
2022-08-02 18:56:55 +0900

    (Feat) 거래 후기 관련 기능 수정 ✨

--- src/main/java/com/seven/marketclip/chat/eums/SellStatus.java
+++ src/main/java/com/seven/marketclip/chat/eums/SellStatus.java
@@ -6,8 +6,9 @@ public enum SellStatus {
     SELLER_TRY("SELLER_TRY"),                // 판매자  버튼 활성화
     BUYER_WAITING("BUY_WAITING"),                 // 구매자  버튼 없음
     //RESERVED
-    TRADE_WAITING("TRADE_WAITING"),           // 판매자  버튼 일시적 비활성화
-    BUYER_CHECK_REQUEST("BUYER_CHECK"),   // 구매자에게 확인 요청 확인 O X 버튼 활성화
+    TRADE_WAITING("TRADE_WAITING"),         // 판매자  버튼 일시적 비활성화
+    BUYER_CHECK_REQUEST("BUYER_CHECK"),     // 구매자에게 확인 요청 확인 O X 버튼 활성화
+    BUYER_STANDBY("BUYER_STANDBY"),         // 구매자 선택 안된 사용자
     //SOLDOUT
     SOLD_OUT("SOLD_OUT");                   // 양 쪽 사용자 버튼 비활성화 채팅 비활성화
 
