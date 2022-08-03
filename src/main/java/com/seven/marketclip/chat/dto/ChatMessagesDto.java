87dcfb157cadfb59d1d93f52ffa8299128a4c6c8
KimjaehoLy <kjaeho8389@naver.com>
2022-08-04 04:45:31 +0900

    (Fix) 메세지 읽음 여부 🐛

--- src/main/java/com/seven/marketclip/chat/dto/ChatMessagesDto.java
+++ src/main/java/com/seven/marketclip/chat/dto/ChatMessagesDto.java
@@ -13,12 +13,14 @@ public class ChatMessagesDto {
     private String chatRoomId;
     private String senderNickname;
     private String message;
+    private boolean checkRead;
     private Date createAt;
 
     public ChatMessagesDto(ChatMessages chatMessages){
         this.messageId = chatMessages.getId();
         this.chatRoomId = chatMessages.getChatRoomId().getId();
         this.senderNickname = chatMessages.getSenderId().getNickname();
+        this.checkRead = chatMessages.isCheckRead();
         this.message = chatMessages.getMessage();
         this.createAt = chatMessages.getCreatedAt();
     }
